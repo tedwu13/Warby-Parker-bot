f01d5d4887653a552f693da9f0c75edfe2f4b9bf
Ted Wu <tedwu2016@u.northwestern.edu>
2017-07-05 21:16:20 -0700

    Add another endpoint

--- server.js
+++ server.js
@@ -10,6 +10,148 @@ app.get('/', function(req, res) {
   res.json({});
 });
 
+app.get('/distance', function(req, res) {
+  distance_dict = { 
+    0: {
+      address: "Shaw, Washington",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/washington-dc/1924-8th-st-nw",
+      lat: 38.916555,
+      lon: -77.023282,
+    },
+    1: {
+      address: "Gold Coast, Chicago",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/chicago/gold-coast",
+      lat: 43.061996,
+      lon: -76.156309,
+    },
+    2: {
+      address: "Armitage Avenue, Chicago",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/chicago/851-w-armitage-ave",
+      lat: 41.917999,
+      lon: -87.650500,
+    },
+    3: {
+      address: "Harber East, Baltimore",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/baltimore/harbor-east",
+      lat: 39.283076,
+      lon: -76.600868,
+    },
+    4: {
+      address: "Newbury St, Boston",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/boston/83-newbury-st",
+      lat: 42.351862,
+      lon: -71.074962,
+    },
+    5: {
+      address: "Woodward Ave, Detroit",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/detroit/1449-woodward-ave",
+      lat: 42.334671,
+      lon: -83.049519,
+    },
+    6: {
+      address: "Country Club Plaza, Kansas City",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/kansas-city/country-club-plaza",
+      lat: 39.041390, 
+      lon: -94.590560,
+    },
+    7: {
+      address: "Greene St, NY",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/new-york/121-greene-st",
+      lat: 40.725447,
+      lon: -73.999264,
+    },
+    8: {
+      address: "Lexington Ave, NY",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/new-york/1209-lexington-ave",
+      lat: 40.776713,
+      lon: -73.957319,
+    },
+    9: {
+      address: "New York City HQ and Showroom, NY",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/new-york/161-avenue-of-americas",
+      lat: 40.725753,
+      lon: -74.004361,
+    },
+    10: {
+      address: "Washington St. NY",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/new-york/819-washington-st",
+      lat: 40.739644,
+      lon: -74.007834,
+    },
+    11: {
+      address: "Walnut St, Philadelphia",
+      image_url: "https://www.wgsn.com/blogs/wp-content/uploads/2013/04/WarbyParker1.jpg",
+      url: "https://www.warbyparker.com/appointments/eye-exams/philadelphia/1523-walnut-st",
+      lat: 39.949927,
+      lon: -75.167005,
+    }
+  }
+
+  var min = 1000000000000;
+  var key;
+  var min_array = [];
+
+  var dist_array = Object.keys(distance_dict).forEach(function(key) {
+    value = distance_dict[key];
+    var req_lat = req.query.latitude;
+    var req_lon = req.query.longitude;
+
+    min_array.push(distance(req_lat, req_lon, value.lat, value.lon));
+  });
+
+  var min_index=min_array.indexOf(Math.min.apply(null,min_array));
+  var closestEyeExam = distance_dict[min_index];
+  res.json({
+     "messages": [
+        {
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "elements":[
+                {
+                  "title": closestEyeExam.address,
+                  "image_url":closestEyeExam.image_url,
+                  "subtitle":"Soft white cotton t-shirt is back in style",
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":closestEyeExam.url,
+                      "title":"Book Appointment"
+                    },
+                  ]
+                },
+              ]
+            }
+          }
+        }
+      ]
+  });
+
+});
+
+function distance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+
 app.get('/sources', function(req, res) {
   request.get({
     'url': 'https://newsapi.org/v1/sources?language=en&country=us',
@@ -80,54 +222,6 @@ app.get('/sources', function(req, res) {
         });
 
       }
-      // res.json({
-      //   "messages": [
-      //      {
-      //        "attachment":{
-      //          "type":"template",
-      //          "payload":{
-      //            "template_type":"generic",
-      //            "elements":[
-      //              {
-      //                "title":"Classic White T-Shirt",
-      //                "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-      //                "subtitle":"Soft white cotton t-shirt is back in style",
-      //                "buttons":[
-      //                  {
-      //                    "type":"web_url",
-      //                    "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-      //                    "title":"View Item"
-      //                  },
-      //                  {
-      //                    "type":"web_url",
-      //                    "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-      //                    "title":"Buy Item"
-      //                  }
-      //                ]
-      //              },
-      //              {
-      //                "title":"Classic Grey T-Shirt",
-      //                "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
-      //                "subtitle":"Soft gray cotton t-shirt is back in style",
-      //                "buttons":[
-      //                  {
-      //                    "type":"web_url",
-      //                    "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
-      //                    "title":"View Item"
-      //                  },
-      //                  {
-      //                    "type":"web_url",
-      //                    "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
-      //                    "title":"Buy Item"
-      //                  }
-      //                ]
-      //              }
-      //            ]
-      //          }
-      //        }
-      //      }
-      //    ]
-      //  });
     }
   });
 });
